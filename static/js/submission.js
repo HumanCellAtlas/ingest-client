1be0174e9beaf7b05a740c2fdeab9ddce0844ef0
Alegria Aclan <aaclan@ebi.ac.uk>
2017-09-26 14:09:39 +0100

    adding copy to clipboard button for staging credentials

--- static/js/submission.js
+++ static/js/submission.js
@@ -1,10 +1,40 @@
-$(function() {
+
+
+$(document).ready(function() {
     renderDates();
+
+    // Requires Bootstrap 3 for functionality
+    $('.js-tooltip').tooltip();
+
+    $('.js-copy').click(function() {
+        var text = $('#staging-credentials').text();
+        console.log(text);
+        var el = $(this);
+        copyToClipboard(text, el);
+    });
 });
 
-function renderDates() {
-    $(".date-column").each(function () {
-        var date = new Date($(this).data("date"));
-        $(this).text(date.toLocaleTimeString() + " " + date.toLocaleDateString());
-    });
+function copyToClipboard(text, el) {
+    var copyTest = document.queryCommandSupported('copy');
+    var elOriginalText = el.attr('data-original-title');
+
+    if (copyTest === true) {
+        var copyTextArea = document.createElement("textarea");
+        copyTextArea.value = text;
+        document.body.appendChild(copyTextArea);
+        copyTextArea.select();
+        try {
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'Copied!' : 'Not copied!';
+            el.attr('data-original-title', msg).tooltip('show');
+        } catch (err) {
+            console.log('Unable to copy');
+        }
+        document.body.removeChild(copyTextArea);
+        el.attr('data-original-title', elOriginalText);
+    } else {
+        // Fallback if browser doesn't support .execCommand('copy')
+        window.prompt("Copy to clipboard: Ctrl+C or Command+C, Enter", text);
+    }
 }
+
