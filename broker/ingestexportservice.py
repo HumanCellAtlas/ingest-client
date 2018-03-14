606a8ba641ee8b723eb443667eb894d2e60df1d0
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-03-14 15:56:23 +0000

    Set up code for bundling Protocol Ingest.

--- broker/ingestexportservice.py
+++ broker/ingestexportservice.py
@@ -394,6 +394,13 @@ class IngestExporter:
             }
         }
 
+    def bundleProtocolIngest(self, protocol_entity):
+        return {
+            'hca_ingest': {
+                'document_id': protocol_entity['uuid']['uuid']
+            }
+        }
+
     def writeMetadataToStaging(self, submissionId, fileName, content, contentType):
         self.logger.info("writing to staging area..." + fileName)
         fileDescription = self.staging_api.stageFile(submissionId, fileName, content, contentType)
