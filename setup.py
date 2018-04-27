b2a08c86d3f75ba0bd16dfa1cb961606b582ac74
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-04-27 12:08:28 +0100

    Defined packaging script for ingest-common.

---
+++ setup.py
@@ -0,0 +1,13 @@
+import os
+from setuptools import setup, find_packages
+
+base_dir = os.path.dirname(__file__)
+install_requires = [line.rstrip() for line in open(os.path.join(base_dir, 'requirements.txt'))]
+
+setup(
+    name = 'hca_ingest',
+    version = '0.1-a',
+    packages = find_packages(exclude=['tests', 'tests.*']),
+    install_requires = install_requires,
+    include_package_data = True
+)
