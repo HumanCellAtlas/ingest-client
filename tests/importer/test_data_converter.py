61bcc15cb03e67a6457b8dc8032b04ddd5e6ad5a
Alegria Aclan <aaclan@ebi.ac.uk>
2018-05-04 15:20:33 +0100

    Refactored tests

--- tests/importer/test_data_converter.py
+++ tests/importer/test_data_converter.py
@@ -5,16 +5,17 @@ from ingest.importer.data_converter import BooleanConverter
 
 class DataConverterTest(TestCase):
 
-    def test_convert(self):
+    def test_convert_to_false(self):
         converter = BooleanConverter()
-
-        self.assertTrue(converter.convert('true'))
-        self.assertTrue(converter.convert('True'))
-        self.assertTrue(converter.convert('yes'))
-        self.assertTrue(converter.convert('Yes'))
-
         self.assertFalse(converter.convert('false'))
         self.assertFalse(converter.convert('False'))
         self.assertFalse(converter.convert('no'))
         self.assertFalse(converter.convert('No'))
+
+    def test_convert_to_true(self):
+        converter = BooleanConverter()
+        self.assertTrue(converter.convert('true'))
+        self.assertTrue(converter.convert('True'))
+        self.assertTrue(converter.convert('yes'))
+        self.assertTrue(converter.convert('Yes'))
 
