6f15cd0c8b64d3e8770a4574e9a468d5d37921cf
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-05-08 14:45:14 +0100

    Set up code for processing string array converter.

--- tests/importer/conversion/test_template_manager.py
+++ tests/importer/conversion/test_template_manager.py
@@ -9,7 +9,7 @@ from ingest.importer.schematemplate import SchemaTemplate
 
 class TemplateManagerTest(TestCase):
 
-    def test_get_converter(self):
+    def test_get_converter_for_string(self):
         # given:
         schema_template = SchemaTemplate()
         short_name_spec = {
@@ -33,3 +33,28 @@ class TemplateManagerTest(TestCase):
 
         # and:
         self.assertIsInstance(converter, Converter)
+
+    def test_get_converter_for_string_array(self):
+        # given:
+        schema_template = SchemaTemplate()
+        names_spec = {
+            'names': {
+                'diplay_name': 'Names',
+                'description': 'List of names',
+                'value_type': 'string',
+                'multivalue': True
+            }
+        }
+        schema_template.lookup = MagicMock(return_value=names_spec)
+
+        # and:
+        template_manager = TemplateManager(schema_template)
+
+        # when:
+        converter = template_manager.get_converter('path.to.field.names')
+
+        # then:
+        schema_template.lookup.assert_called_with('path.to.field.names')
+
+        # and:
+        self.assertIsNotNone(converter)
