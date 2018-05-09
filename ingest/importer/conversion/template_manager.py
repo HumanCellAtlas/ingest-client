6cbba4f293065f55218a483c9477ca69d83c97f4
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-05-09 11:02:32 +0100

    Defined code to resolve column spec to integer converter.

--- ingest/importer/conversion/template_manager.py
+++ ingest/importer/conversion/template_manager.py
@@ -1,4 +1,4 @@
-from ingest.importer.conversion.data_converter import Converter, ListConverter
+from ingest.importer.conversion.data_converter import Converter, ListConverter, IntegerConverter
 from ingest.template.schematemplate import SchemaTemplate
 
 
@@ -9,8 +9,14 @@ class TemplateManager:
 
     def get_converter(self, header_name):
         column_spec = self.template.lookup(header_name)
-        if (column_spec.get('multivalue')):
+
+        if column_spec.get('multivalue'):
             converter = ListConverter()
         else:
             converter = Converter()
+
+        value_type = column_spec.get('value_type')
+        if value_type == 'integer':
+            converter = IntegerConverter()
+
         return converter
--- tests/importer/conversion/test_template_manager.py
+++ tests/importer/conversion/test_template_manager.py
@@ -2,7 +2,8 @@ from unittest import TestCase
 
 from mock import MagicMock
 
-from ingest.importer.conversion.data_converter import Converter, ListConverter, DataType
+from ingest.importer.conversion.data_converter import Converter, ListConverter, DataType, \
+    IntegerConverter
 from ingest.importer.conversion.template_manager import TemplateManager
 from ingest.template.schematemplate import SchemaTemplate
 
@@ -17,6 +18,7 @@ def _mock_schema_template_lookup(value_type='string', multivalue=False):
     return schema_template
 
 
+# TODO make the processing of value type case insensitive
 class TemplateManagerTest(TestCase):
 
     def test_get_converter_for_string(self):
@@ -45,3 +47,15 @@ class TemplateManagerTest(TestCase):
         schema_template.lookup.assert_called_with(header_name)
         self.assertIsInstance(converter, ListConverter)
         self.assertEqual(DataType.STRING, converter.base_type)
+
+    def test_get_converter_for_integer(self):
+        # given:
+        schema_template = _mock_schema_template_lookup(value_type='integer')
+        template_manager = TemplateManager(schema_template)
+
+        # when:
+        header_name = 'path.to.field'
+        converter = template_manager.get_converter(header_name)
+
+        # then:
+        self.assertIsInstance(converter, IntegerConverter)
