5a1fb92a293adb913fc27625e1fe349b2015d08d
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-05-09 12:04:27 +0100

    Minor refactoring.

--- ingest/importer/conversion/template_manager.py
+++ ingest/importer/conversion/template_manager.py
@@ -10,10 +10,14 @@ class TemplateManager:
     def get_converter(self, header_name):
         column_spec = self.template.lookup(header_name)
         default_converter = CONVERTER_MAP[DataType.STRING]
-        value_type = column_spec.get('value_type')
-        data_type = DataType.find(value_type)
-        if column_spec.get('multivalue'):
+        data_type = self._resolve_data_type(column_spec)
+        if column_spec.get('multivalue', False):
             converter = ListConverter(data_type=data_type)
         else:
             converter = CONVERTER_MAP.get(data_type, default_converter)
         return converter
+
+    def _resolve_data_type(self, column_spec):
+        value_type = column_spec.get('value_type')
+        data_type = DataType.find(value_type)
+        return data_type
