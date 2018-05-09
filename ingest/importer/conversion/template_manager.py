a1f74fbffa41e0b32b1872d992a79078ce1470ed
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-05-09 12:00:21 +0100

    Refactored code to use converter mapping.

--- ingest/importer/conversion/template_manager.py
+++ ingest/importer/conversion/template_manager.py
@@ -1,5 +1,4 @@
-from ingest.importer.conversion.data_converter import Converter, ListConverter, IntegerConverter, \
-    DataType
+from ingest.importer.conversion.data_converter import ListConverter, DataType, CONVERTER_MAP
 from ingest.template.schematemplate import SchemaTemplate
 
 
@@ -10,16 +9,11 @@ class TemplateManager:
 
     def get_converter(self, header_name):
         column_spec = self.template.lookup(header_name)
-
-        if column_spec.get('multivalue'):
-            converter = ListConverter()
-        else:
-            converter = Converter()
-
+        default_converter = CONVERTER_MAP[DataType.STRING]
         value_type = column_spec.get('value_type')
-        if value_type == 'integer':
-            converter = IntegerConverter()
-            if column_spec.get('multivalue'):
-                converter = ListConverter(data_type=DataType.INTEGER)
-
+        data_type = DataType.find(value_type)
+        if column_spec.get('multivalue'):
+            converter = ListConverter(data_type=data_type)
+        else:
+            converter = CONVERTER_MAP.get(data_type, default_converter)
         return converter
