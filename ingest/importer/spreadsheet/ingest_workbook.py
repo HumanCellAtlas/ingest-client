fa58d2f53809fee6eb480c36b189cd0a92d8797b
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-05-14 11:03:42 +0100

    Extracted worksheet name into a constant.

--- ingest/importer/spreadsheet/ingest_workbook.py
+++ ingest/importer/spreadsheet/ingest_workbook.py
@@ -1,5 +1,7 @@
 from openpyxl import Workbook
 
+SCHEMAS_WORKSHEET = 'schemas'
+
 
 class IngestWorkbook:
 
@@ -7,7 +9,7 @@ class IngestWorkbook:
         self.workbook = workbook
 
     def get_schemas(self):
-        worksheet = self.workbook.get_sheet_by_name('schemas')
+        worksheet = self.workbook.get_sheet_by_name(SCHEMAS_WORKSHEET)
         schemas = []
         for row in worksheet.iter_rows(row_offset=1, max_row=(worksheet.max_row - 1)):
             schema_cell = row[0]
@@ -16,5 +18,5 @@ class IngestWorkbook:
 
     def importable_worksheets(self):
         importable_names = [name for name in self.workbook.get_sheet_names() if
-                            (not name == 'schemas')]
+                            (not name == SCHEMAS_WORKSHEET)]
         return [self.workbook.get_sheet_by_name(name) for name in importable_names]
