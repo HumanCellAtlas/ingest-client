7ae47934320826a22ad5bf1cc9c61d1df03558d8
Alegria Aclan <aaclan@ebi.ac.uk>
2018-05-14 18:50:11 +0100

    increment version - fix to export code - convert dict values to list

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ install_requires = [line.rstrip() for line in open(os.path.join(base_dir, 'requi
 
 setup(
     name = 'hca_ingest',
-    version = '0.3-b',
+    version = '0.4',
     packages = find_packages(exclude=['tests', 'tests.*']),
     install_requires = install_requires,
     include_package_data = True
