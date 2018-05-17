d30bc9b07e16d9e26ee8ccb3828974c8cb9982e8
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-05-17 10:55:54 +0100

    Set up code for handling insert of data to previously processed field.

--- tests/importer/conversion/test_conversion_strategy.py
+++ tests/importer/conversion/test_conversion_strategy.py
@@ -27,14 +27,17 @@ class DirectCellConversionTest(TestCase):
         self.assertEqual(27, user.get('age'))
 
 
+def _create_mock_string_converter():
+    converter = MagicMock('converter')
+    converter.convert = lambda data: f'{data} - converted'
+    return converter
+
+
 class ListElementCellConversionTest(TestCase):
 
     def test_apply(self):
         # given:
-        converter = MagicMock('converter')
-        converter.convert = lambda data: f'{data} - converted'
-
-        # and:
+        converter = _create_mock_string_converter()
         cell_conversion = ListElementCellConversion('list_of_things.name', converter)
 
         # when:
@@ -49,3 +52,19 @@ class ListElementCellConversionTest(TestCase):
         # and:
         thing = list_of_things[0]
         self.assertEqual('sample - converted', thing.get('name'))
+
+    def test_apply_previously_processed_field(self):
+        # given:
+        converter = _create_mock_string_converter()
+        cell_conversion = ListElementCellConversion('user.basket.product_name', converter)
+
+        # and:
+        data_node = DataNode()
+        data_node['user.basket'] = [{'quantity': 3}]
+
+        # when:
+        cell_conversion.apply(data_node, 'apple')
+
+        # then:
+        basket = data_node.as_dict().get('user').get('basket')
+        self.assertEqual(1, len(basket))
