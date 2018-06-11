e446e1f01dcf74c24ea29b77f496872c617fabfa
Simon Jupp <simon.jupp@gmail.com>
2018-06-11 09:46:49 +0100

    added boolean retrievable to all prpoerties, will only return true for <schema name>.uuid.retrievable

--- tests/template/test_schematemplate.py
+++ tests/template/test_schematemplate.py
@@ -25,6 +25,9 @@ class TestSchemaTemplate(TestCase):
         self.dummyDonorUri = "https://schema.humancellatlas.org/type/biomaterial/5.1.0/donor_organism"
         pass
 
+    def test_no_schemas(self):
+        schema_template = SchemaTemplate()
+
 
     def test_schema_lookup(self):
 
@@ -129,6 +132,17 @@ class TestSchemaTemplate(TestCase):
         template = schema_mock.get_template_for_json(data=data)
         self.assertIsNone(template.lookup("donor_organism.foo_bar.format"))
 
+    def test_retrieveable(self):
+        data = '{"id" : "' + self.dummyDonorUri + '", "properties": {"foo_bar": {"format" : "date-time"}} }'
+        template = schema_mock.get_template_for_json(data=data)
+        self.assertTrue(template.lookup("donor_organism.uuid.retrievable"))
+
+        with self.assertRaises(UnknownKeyException):
+            self.assertTrue(template.lookup("donor_organism.format.uuid.retrievable"))
+
+        data = '{"id" : "' + self.dummyDonorUri + '", "properties": {"foo_bar": {"user_friendly" : "Foo bar"}} }'
+        template = schema_mock.get_template_for_json(data=data)
+        self.assertIsNone(template.lookup("donor_organism.foo_bar.format"))
 
     def test_example(self):
         data = '{"id" : "' + self.dummyDonorUri + '", "properties": {"foo_bar": {"example" : "Foo is a bar"}} }'
