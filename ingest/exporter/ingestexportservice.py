b280b1ac5c7d3efc3f0acac66014e5c6212f260a
Rolando Fernandez <rolando@ebi.ac.uk>
2018-06-21 19:23:37 +0100

    added a describedBy for bundle metadata documents

--- ingest/exporter/ingestexportservice.py
+++ ingest/exporter/ingestexportservice.py
@@ -188,6 +188,7 @@ class IngestExporter:
         bundle_doc['content'] = metadata_doc['content']
         bundle_doc['create_date'] = metadata_doc['submissionDate']
         bundle_doc['update_date'] = metadata_doc['updateDate']
+        bundle_doc['describedBy'] = "https://raw.githubusercontent.com/HumanCellAtlas/ingest-exporter/unjumble/schema/bundle_schema.json"
 
         return bundle_doc
 
