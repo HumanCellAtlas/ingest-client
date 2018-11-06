b78c642e403e443b3935bb4a94be460142b311ac
Rolando Fernandez <rolando@ebi.ac.uk>
2018-11-06 16:28:32 -0500

    figuring the project_id from the jwt_auth info dict

--- ingest/utils/s2s_token_client.py
+++ ingest/utils/s2s_token_client.py
@@ -7,11 +7,9 @@ class S2STokenClient:
     def __init__(self, dcp_auth_client: 'DCPAuthClient'=None):
         self.dcp_auth_client = dcp_auth_client
 
-    def setup_from_env_var(self, env_var_name, google_project):
+    def setup_from_env_var(self, env_var_name):
         key_dict = json.loads(os.environ.get(env_var_name))
-        self.dcp_auth_client = DCPAuthClient(google_project, key_dict)
+        self.dcp_auth_client = DCPAuthClient(key_dict["project_id"], key_dict)
 
     def retrieve_token(self) -> str:
         return self.dcp_auth_client.token
-
-
