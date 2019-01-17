1e49f2cd2c00926cb91d862dae9326385f2653cf
Rodrey Goite <rodrey@ebi.ac.uk>
2019-01-17 13:02:45 +0000

    Converted env var value to int.

--- ingest/api/requests_utils.py
+++ ingest/api/requests_utils.py
@@ -4,7 +4,7 @@ import requests
 from requests.adapters import HTTPAdapter
 
 env_max_retries = os.environ.get('REQUESTS_MAX_RETRIES')
-DEFAULT_MAX_RETRIES = env_max_retries if env_max_retries else 3
+DEFAULT_MAX_RETRIES = int(env_max_retries) if env_max_retries else 3
 
 
 def optimistic_session(url_prefix, max_retries=DEFAULT_MAX_RETRIES):
