48c9216ba5312c8e87f9c3bfe726a4db12f4775f
Rodrey Goite <rodrey@ebi.ac.uk>
2019-02-11 09:55:16 +0000

    Defined concrete and domain type fields.

--- ingest/importer/conversion/metadata_entity.py
+++ ingest/importer/conversion/metadata_entity.py
@@ -2,15 +2,30 @@ import copy
 
 from ingest.importer.data_node import DataNode
 
+TYPE_UNDEFINED = 'undefined'
+
 
 class MetadataEntity:
 
-    def __init__(self, object_id=None, content={}, links={}, external_links={}, linking_details={}):
+    # TODO enforce definition of concrete and domain types for all MetadataEntity
+    # It's only currently done this way to minimise friction with other parts of the system
+    def __init__(self, concrete_type=TYPE_UNDEFINED, domain_type=TYPE_UNDEFINED, object_id=None,
+                 content={}, links={}, external_links={}, linking_details={}):
+        self._concrete_type = concrete_type
+        self._domain_type = domain_type
         self.object_id = object_id
         self._content = DataNode(defaults=copy.deepcopy(content))
         self._links = copy.deepcopy(links)
         self._external_links = copy.deepcopy(external_links)
         self._linking_details = DataNode(defaults=copy.deepcopy(linking_details))
+
+    @property
+    def concrete_type(self):
+        return self._concrete_type
+
+    @property
+    def domain_type(self):
+        return self._domain_type
 
     @property
     def content(self):
