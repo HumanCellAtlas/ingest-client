7039b52b521fb908be802f75a4c3baf053b5cd64
Rodrey Goite <rodrey@ebi.ac.uk>
2019-02-26 13:15:55 +0000

    Minor update to documentation.

--- ingest/importer/conversion/template_manager.py
+++ ingest/importer/conversion/template_manager.py
@@ -91,11 +91,11 @@ class TemplateManager:
 
     def get_concrete_type(self, title):
         """
-        Concrete Entity refers to the specific type of an object based on a given schema.
+        Concrete Type refers to the specific type of an object based on a given schema.
         This method determines the concrete type given the worksheet title.
 
         :param title: the title of the worksheet.
-        :return: the Concrete Entity of a given worksheet title
+        :return: the Concrete Type of a given worksheet title
         """
         result = MODULE_TITLE_PATTERN.search(title)
         if not result:
