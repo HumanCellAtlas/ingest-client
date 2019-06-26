3b1dea5d09bc3a22cd0274a75d54899dca263b18
Rolando Fernandez <rolando@ebi.ac.uk>
2019-06-26 11:08:13 +0100

    explicified logic for ignoring attempts to add links.json to BundleManifests

--- ingest/exporter/bundle.py
+++ ingest/exporter/bundle.py
@@ -31,9 +31,11 @@ class BundleManifest:
         self.fileProtocolMap = {}
 
     def add_bundle_file(self, metadata_type, entry: dict):
-        if metadata_type == _BUNDLE_FILE_TYPE_DATA:
+        if metadata_type == _BUNDLE_FILE_TYPE_LINKS:
+            pass  # we don't want to track links.json in BundleManifests
+        elif metadata_type == _BUNDLE_FILE_TYPE_DATA:
             self.dataFiles.extend(entry.keys())
-        elif metadata_type != _BUNDLE_FILE_TYPE_LINKS:
+        else:
             attr_mapping = _metadata_type_attr_map.get(metadata_type)
             if attr_mapping:
                 file_map = getattr(self, attr_mapping)
