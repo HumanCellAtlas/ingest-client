8cac3d28fd52ad5ccd3539cce38b0aca69a73ca4
Alexie Staffer <astaffer@ebi.ac.uk>
2019-08-19 11:33:34 +0100

    Fixup SubmissionError class.

--- ingest/utils/SubmissionError.py
+++ ingest/utils/SubmissionError.py
@@ -3,10 +3,10 @@ class SubmissionError:
     A SubmissionError that can be reported to ingest.
     """
 
-    def __init__(self, errorType="http://ingest.data.humancellatlas.org/Error", errorTitle="", errorDetail=""):
-        self.type = errorType
-        self.title = errorTitle
-        self.detail = errorDetail
+    def __init__(self):
+        self.type = "http://ingest.data.humancellatlas.org/Error"
+        self.title = ""
+        self.detail = ""
 
     def getJSON(self):
         return {
@@ -21,10 +21,10 @@ class ImporterError(SubmissionError):
     An error importing the Submission, to be reported to ingest.
     """
 
-    def __init__(self, errorDetail=""):
+    def __init__(self, detail=""):
         self.type = "http://importer.ingest.data.humancellatlas.org/Error"
         self.title = "An error occurred importing the submission."
-        self.detail = errorDetail
+        self.detail = detail
 
 
 class ExporterError(SubmissionError):
@@ -32,7 +32,7 @@ class ExporterError(SubmissionError):
     An error exporting the Submission, to be reported to ingest.
     """
 
-    def __init__(self, errorDetail=""):
+    def __init__(self, detail=""):
         self.type = "http://exporter.ingest.data.humancellatlas.org/Error"
         self.title = "Error occurred while attempting to export the submission."
-        self.detail = errorDetail
+        self.detail = detail
