28161c20d178dd928932c6922e3c5050226b27fd
Alegria Aclan <aaclan@ebi.ac.uk>
2019-09-24 10:17:08 +0100

    Moved the spreadsheet upload error in the spreadsheet upload service in ingest-broker repo

--- ingest/importer/spreadsheetUploadError.py
+++
@@ -1,5 +0,0 @@
-class SpreadsheetUploadError(Exception):
-    def __init__(self, http_code, message, details=None):
-        self.http_code = http_code
-        self.message = message
-        self.details = details
